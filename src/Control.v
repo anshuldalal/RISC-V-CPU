b0d8ab219b012005375c0e65f9666ef00517e22b
anshuldalal <34630957+anshuldalal@users.noreply.github.com>
2020-11-11 20:32:07 +0530

    Update Control.v

--- src/Control.v
+++ src/Control.v
@@ -1,76 +1,62 @@
-module Control(
-    Op_i       ,
-    ALUOp_o    ,
-    ALUSrc_o   ,
-    RegWrite_o ,
-    MemRd_o,
-    MemWr_o,
-    MemToReg_o,
-    immSelect_o
+module Control
+(    
+    input       [6:0]  Op_i;
+    output  reg [1:0]  ALUOp_o;
+    output  reg        ALUSrc_o,immSelect_o;
+    output  reg        RegWrite_o ,MemRd_o,MemWr_o,MemToReg_o;
 );
 
-//ports
-
-input [6:0]     Op_i;
-output reg[1:0]     ALUOp_o;
-output  reg      ALUSrc_o,immSelect_o;
-output  reg    RegWrite_o ,MemRd_o,MemWr_o,MemToReg_o;
-
-
-
-
 always@(*)begin
-
-  case(Op_i)
+ case(Op_i)
 
   7'b0010011 : begin //addi
-  ALUOp_o = 2'b11;
-  ALUSrc_o = 1'b1;
-  RegWrite_o = 1'b1;
-  MemRd_o = 1'b0;
-  MemWr_o = 1'b0;
-  MemToReg_o = 1'b0;
-  immSelect_o = 1'b0;
-  end
-  
+                    ALUOp_o = 2'b11;
+                    ALUSrc_o = 1'b1;
+                    RegWrite_o = 1'b1;
+                    MemRd_o = 1'b0;
+                    MemWr_o = 1'b0;
+                    MemToReg_o = 1'b0;
+                    immSelect_o = 1'b0;
+               end
+      
   7'b0110011 : begin //others
-  ALUOp_o = 2'b10;
-  ALUSrc_o = 1'b0;
-  RegWrite_o = 1'b1;
-  MemRd_o = 1'b0;
-  MemWr_o = 1'b0;
-  MemToReg_o = 1'b0;
-  immSelect_o = 1'b0;
-  end
+                    ALUOp_o = 2'b10;
+                    ALUSrc_o = 1'b0;
+                    RegWrite_o = 1'b1;
+                    MemRd_o = 1'b0;
+                    MemWr_o = 1'b0;
+                    MemToReg_o = 1'b0;
+                    immSelect_o = 1'b0;
+               end
 
   7'b1100011 : begin //beq
-  ALUOp_o = 2'b01;
-  ALUSrc_o = 1'b1;
-  RegWrite_o = 1'b0;
-  MemRd_o = 1'b0;
-  MemWr_o = 1'b0;
-  MemToReg_o = 1'b0;
-  immSelect_o = 1'b0;
-  end
+                    ALUOp_o = 2'b01;
+                    ALUSrc_o = 1'b1;
+                    RegWrite_o = 1'b0;
+                    MemRd_o = 1'b0;
+                    MemWr_o = 1'b0;
+                    MemToReg_o = 1'b0;
+                    immSelect_o = 1'b0;
+               end
 
   7'b0000011 : begin //lw
-  ALUOp_o = 2'b00;
-  ALUSrc_o = 1'b1;
-  MemRd_o = 1'b1;
-  MemToReg_o = 1'b1;
-  RegWrite_o = 1'b1;
-  MemWr_o = 1'b0;
-  immSelect_o = 1'b0;
-  end
+                    ALUOp_o = 2'b00;
+                    ALUSrc_o = 1'b1;
+                    MemRd_o = 1'b1;
+                    MemToReg_o = 1'b1;
+                    RegWrite_o = 1'b1;
+                    MemWr_o = 1'b0;
+                    immSelect_o = 1'b0;
+               end
 
   7'b0100011 : begin //sw
-  ALUOp_o = 2'b00;
-  ALUSrc_o = 1'b1;
-  MemWr_o = 1'b1;
-  RegWrite_o = 1'b0;
-  MemRd_o = 1'b0;
-  MemToReg_o = 1'b0;
-  immSelect_o = 1'b1;
+                    ALUOp_o = 2'b00;
+                    ALUSrc_o = 1'b1;
+                    MemWr_o = 1'b1;
+                    RegWrite_o = 1'b0;
+                    MemRd_o = 1'b0;
+                    MemToReg_o = 1'b0;
+                    immSelect_o = 1'b1;
   end
 
   //---------NEW-----------
@@ -94,6 +80,6 @@ always@(*)begin
   MemToReg_o = 1'b0;
   immSelect_o = 1'b0;
   end
-  endcase
+ endcase
 end
 endmodule
