c1d9b6554defd230696f971b08ad882c24f01425
anshuldalal <34630957+anshuldalal@users.noreply.github.com>
2020-11-11 18:25:44 +0530

    Update DataMemory.v

--- src/DataMemory.v
+++ src/DataMemory.v
@@ -1,41 +1,29 @@
 module Data_Memory
-(   
-    clk_i,
-    reset,
-    op_addr,
-    addr_i,
-    data_i,
-    MemWrite_i,
-    MemRead_i,
-    data_o,
-    data_mem_o
+(
+    input           clk_i,
+    input           reset,
+    input  [31:0]   addr_i,
+    input  [31:0]   data_i,
+    input           MemWrite_i, MemRead_i,
+    output [31:0]   data_o
 );
-
-
-input clk_i;
-input reset;
-input [4:0] op_addr;
-input [31:0] addr_i,data_i;
-input MemWrite_i, MemRead_i;
+    
 integer i;
-output [31:0] data_o;
-output [31:0] data_mem_o;
-
-reg [7:0] memory [0:31];
+reg [7:0] memory [0:31];    // 8 x 32
 wire [31:0] op;
 
-assign op = { memory[addr_i + 3],memory[addr_i + 2],
-memory[addr_i + 1],memory[addr_i]};
-
-assign data_mem_o = { memory[op_addr + 3],memory[op_addr + 2],
-memory[op_addr + 1],memory[op_addr]};
-
+// Read, by default on posedges (as soon as addr_i updated)
 assign data_o = (MemRead_i) ? op : 32'b0;
+assign op = { memory[addr_i + 3], memory[addr_i + 2], memory[addr_i + 1], memory[addr_i]};
 
-always @(posedge clk_i or posedge reset) begin
-
-    if(reset)for(i=0;i<32;i=i+1)memory[i] <= 0;
-     else if(MemWrite_i) begin
+// Write   // Write on negedges to avoid write and read on the same cycle
+always @(negedge clk_i or posedge reset) 
+begin
+    if(reset)
+        for(i=0;i<32;i=i+1)
+            memory[i] <= 0;
+     else 
+         if(MemWrite_i) begin
             memory[addr_i + 3] <= data_i[31:24];
             memory[addr_i + 2] <= data_i[23:16];
             memory[addr_i + 1] <= data_i[15:8];
@@ -44,4 +32,3 @@ always @(posedge clk_i or posedge reset) begin
 end
 
 endmodule
-
