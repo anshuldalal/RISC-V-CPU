ed254591c99e19c08999c76e1763cf4f89d70dd5
anshuldalal <34630957+anshuldalal@users.noreply.github.com>
2020-11-11 19:58:14 +0530

    Need to include code to enable overwriting memory
    Need to include code to enable overwriting memory

--- src/Instruction_Memory.v
+++ src/Instruction_Memory.v
@@ -1,49 +1,49 @@
 module Instruction_Memory
 (
-    clk,
-    reset,
-    addr_i, 
-    instr_i,
-    instr_o
+    input               clk,
+    input               reset,
+    input   [31:0]      addr_i,     // Instruction address for Read/Write (PC while reading) 
+    input               write_inst_i,
+    input   [7:0]       instr_i,    // Write these instructions to instruction-memory
+    output  [31:0]      instr_o     // fetched 32-bit instruction
 );
-integer i;
-// Interface
-input               clk;
-input               reset;
-input   [31:0]      addr_i;
-input   [7:0]       instr_i;
-output  [31:0]      instr_o;
-
+    
 // Instruction memory
-reg     [31:0]     memory  [0:63];
-reg     [1:0]      quad,quad_d1;
+reg     [31:0]     memory  [0:63];  // 32 x 64 Instruction memory
 reg     [7:0]      instr_read;
 reg     [5:0]      address_read;
 reg                flag,flag_next;
 reg     [1:0]      counter,counter_next;
 reg     [5:0]      instr_wr_address,instr_wr_address_next;
 
-assign  instr_o = memory[addr_i>>2];  
+assign  instr_o = memory[addr_i>>2];  // fetched instruction, doubtful about memory-alignment
 
-always@(*)begin
-    if(instr_i == 8'b1111_1110)flag_next = 1;
-    else if (instr_i == 8'b1111_1111)flag_next = 0;
-    else flag_next = flag;
+always@(*)
+begin
+    if  (instr_i == 8'b1111_1110)      // start of program
+        flag_next = 1;
+    else if (instr_i == 8'b1111_1111)  // end of program 
+        flag_next = 0;
+    else 
+        flag_next = flag;
 
-    if(flag)counter_next = counter + 2'd1;
-    else counter_next = counter;
+    if(flag)
+        counter_next = counter + 2'd1; // read next 8 bits of the instruction address
+    else 
+        counter_next = counter;
 
-    if(counter == 2'b11)instr_wr_address_next = instr_wr_address + 6'd1;
-    else instr_wr_address_next = instr_wr_address;
+    if(counter == 2'b11)
+        instr_wr_address_next = instr_wr_address + 6'd1;    // write next instruction
+    else 
+        instr_wr_address_next = instr_wr_address;
 end
 
+integer i;
+
+// Overwriting the instruction memory with new program    
 always@(posedge clk or posedge reset)begin
     if(reset)begin
-        for(i=0;i<63;i=i+1)begin
-            memory[i] <= 0;
-        end
         counter         <= 0;
-        quad            <= 0;
         instr_read      <= 0;
         flag            <= 0;
         address_read    <= 0;
@@ -52,21 +52,18 @@ always@(posedge clk or posedge reset)begin
     else begin
         flag             <= flag_next;
         counter          <= counter_next;
-        quad             <= (2'b11-counter);
-        quad_d1          <= quad;
         instr_wr_address <= instr_wr_address_next;
         address_read     <= instr_wr_address;
-        instr_read       <= instr_i;
+        instr_read       <= instr_i;    // 8 bits of instruction
         
-        
-        if(flag)begin
-            case(quad)
-            2'b00: memory[address_read][7:0]   <= (instr_read == 8'b1111_1111)?0:instr_read;
-            2'b01: memory[address_read][15:8]  <= (instr_read == 8'b1111_1111)?0:instr_read;
-            2'b10: memory[address_read][23:16] <= (instr_read == 8'b1111_1111)?0:instr_read;
-            2'b11: memory[address_read][31:24] <= (instr_read == 8'b1111_1111)?0:instr_read;
-            default: memory[address_read][7:0] <= (instr_read == 8'b1111_1111)?0:instr_read;
-            endcase
+        if(flag) begin
+          case(counter)
+            2'b00:   memory[address_read][31:24] <= (instr_read == 8'b1111_1111)?0:instr_read;
+            2'b01:   memory[address_read][23:16] <= (instr_read == 8'b1111_1111)?0:instr_read;
+            2'b10:   memory[address_read][15:8]  <= (instr_read == 8'b1111_1111)?0:instr_read;
+            2'b11:   memory[address_read][7:0]   <= (instr_read == 8'b1111_1111)?0:instr_read;              
+            default: memory[address_read][7:0]   <= (instr_read == 8'b1111_1111)?0:instr_read;
+          endcase
         end
     end
 end
