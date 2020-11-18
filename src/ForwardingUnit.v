521f1d2714aa320d0dd65d43a0c75d68cd669acb
anshuldalal <34630957+anshuldalal@users.noreply.github.com>
2020-11-18 11:42:43 +0530

    Update ForwardingUnit.v

--- src/ForwardingUnit.v
+++ src/ForwardingUnit.v
@@ -13,14 +13,12 @@ always@(*)begin
     ForwardA_o = 2'b00;
     
     // Forwarding from EX stage
-    if(EX_MEM_RegWrite_i && (EX_MEM_RD_i != 5'b00000) && 
-      (EX_MEM_RD_i == ID_EX_RS_i)) 
+    if(EX_MEM_RegWrite_i && (EX_MEM_RD_i != 5'b00000) && (EX_MEM_RD_i == ID_EX_RS_i)) 
             ForwardA_o = 2'b10;
     
     // Forwarding from MEM stage
     else if (MEM_WB_RegWrite_i && (MEM_WB_RD_i != 5'b00000) &&
-             ~(EX_MEM_RegWrite_i && (EX_MEM_RD_i != 5'b000000) &&   // conflict of forwarding when RD in EX and MEM stage are same
-               (EX_MEM_RD_i == ID_EX_RS_i)) &&
+             ~(EX_MEM_RegWrite_i && (EX_MEM_RD_i != 5'b000000) && (EX_MEM_RD_i == ID_EX_RS_i)) && // conflict of forwarding when RD in EX and MEM stage are same
              MEM_WB_RD_i == ID_EX_RS_i)  
                 ForwardA_o = 2'b01;
 end
@@ -30,14 +28,12 @@ always@(*)begin
     ForwardB_o = 2'b00;
  
     // Forwarding from EX stage   
-    if(EX_MEM_RegWrite_i && (EX_MEM_RD_i != 5'b00000) && 
-      (EX_MEM_RD_i == ID_EX_RT_i)) 
+    if(EX_MEM_RegWrite_i && (EX_MEM_RD_i != 5'b00000) && (EX_MEM_RD_i == ID_EX_RT_i)) 
            ForwardB_o = 2'b10;
     
     // Forwarding from MEM stage
     else if (MEM_WB_RegWrite_i && (MEM_WB_RD_i != 5'b00000) &&
-             ~(EX_MEM_RegWrite_i && (EX_MEM_RD_i != 5'b000000) &&   // conflict of forwarding when RD in EX and MEM stage are same
-               (EX_MEM_RD_i == ID_EX_RS_i)) &&
+             ~(EX_MEM_RegWrite_i && (EX_MEM_RD_i != 5'b000000) &&  (EX_MEM_RD_i == ID_EX_RS_i)) && // conflict of forwarding when RD in EX and MEM stage are same
              MEM_WB_RD_i == ID_EX_RT_i)  
                 ForwardB_o = 2'b01;
 end
