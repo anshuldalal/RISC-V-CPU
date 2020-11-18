fe42413c098d205f70039c81ca68bd82aede1928
anshuldalal <34630957+anshuldalal@users.noreply.github.com>
2020-11-18 18:21:14 +0530

    Update ALU.v

--- src/ALU.v
+++ src/ALU.v
@@ -1,47 +1,53 @@
-module ALU (data1_i, data2_i, ALUCtrl_i, data_o,Zero_o);
-
-input [31:0] data1_i, data2_i;
-input [2:0] ALUCtrl_i;
-output reg[31:0] data_o;
-output reg Zero_o;
-
-parameter SUM = 3'b001;
+module ALU 
+(  
+  input       [31:0]  data1_i, data2_i,
+  //input       [ : ]   shamt,
+  input       [2:0]   ALUCtrl_i,
+  output reg  [31:0]  data_o,
+  output reg          Zero_o
+);
+  
+parameter ADD = 3'b001;
 parameter SUB = 3'b010;
-parameter AND = 3'b011;
-parameter OR  = 3'b100;
+parameter SLL = 3'b010;
 parameter XOR = 3'b101;
-parameter MUL = 3'b110;
+parameter SLR = 3'b010;
+parameter SRA = 3'b010;
+parameter OR  = 3'b100;
+parameter AND = 3'b011;
+parameter BLT = 3'b110;
+parameter BGE  = 3'b100;
 
 
-/* implement here */
 always@(*)begin
-Zero_o   = (data1_i - data2_i)?0:1;
-case(ALUCtrl_i)
-
-  SUM : begin
-    data_o = data1_i + data2_i;
-  end
-  SUB : begin
-    data_o = data1_i - data2_i;
-  end
-  AND : begin
-    data_o = data1_i & data2_i;
-  end
-  OR : begin
-    data_o = data1_i | data2_i;
-  end
-  XOR : begin
-    data_o = data1_i ^ data2_i;
-  end
-  MUL : begin
-    data_o = data1_i * data2_i;
-  end
-  default : begin
-    data_o = data1_i;
-  end
-
-endcase
+  
+  case(ALUCtrl_i)
+    
+      ADD     :  data_o = data1_i + data2_i;
+      SUB     :  begin 
+                    // Zero_o   = (data1_i - data2_i)?0:1;
+                    data_o = data1_i - data2_i; 
+                 end
+      SLL     :  data_o = data1_i << data2_i;
+      XOR     :  data_o = data1_i ^ data2_i;
+      SLR     :  data_o = data1_i >> data2_i;
+      SRA     :  data_o = data1_i  data2_i;
+      OR      :  data_o = data1_i | data2_i;
+      AND     :  data_o = data1_i & data2_i;
+      BLT     :  begin 
+                    // signed
+                    // Zero_o   = (data1_i - data2_i)?0:1;
+                    data_o = data1_i - data2_i; 
+                 end
+      BGE     :  begin 
+                    // signed
+                    // Zero_o   = (data1_i - data2_i)?0:1;
+                    data_o = data1_i - data2_i; 
+                 end
+      default : data_o = data1_i;
+    
+  endcase
 
 end
-
+  
 endmodule
