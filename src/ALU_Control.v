293f93ad275eca8ece9b49405cbe6b281f36b985
anshuldalal <34630957+anshuldalal@users.noreply.github.com>
2020-11-20 17:58:45 +0530

    Update ALU_Control.v

--- src/ALU_Control.v
+++ src/ALU_Control.v
@@ -2,46 +2,40 @@ module ALU_Control
 (
     input       [9:0] funct_i,  //   funct7 = funct_i[9:3], funct3 = funct_i[2:0]
     input       [1:0] ALUOp_i,  //   00 for addi
-    output reg  [2:0] ALUCtrl_o
+    output reg  [3:0] ALUCtrl_o
 );
     
 always@(*)begin
 
   case(ALUOp_i)
-
-    2'b11 : begin   // immediate ?????????????  // should be removed
-               case(funct_i[2:0])
-                    3'000 : ALUCtrl_o = 3'b;    // ADDI                          
-                    3'001 : ALUCtrl_o = 3'b;    // SLLI
-                    3'100 : ALUCtrl_o = 3'b;    // XORI
-                    3'101 : ALUCtrl_o = (funct_i[9:3] == 0) ? 3'b : 3'b ;    // SRLI : SRAI
-                    3'110 : ALUCtrl_o = 3'b;    // ORI
-                    3'111 : ALUCtrl_o = 3'b;    // ANDI
-                    default         : ALUCtrl_o = 3'b001;
-               endcase
-            end
-            
-    2'b10 : begin   // R-Type and I - Type too ????????????????? 
-                case(funct_i)
-                    10'b0000000_000 : ALUCtrl_o = 3'b001;    // ADD                          
-                    10'b0100000_000 : ALUCtrl_o = 3'b010;    // SUB
-                    10'b0000000_001 : ALUCtrl_o = 3'b;       // SLL
-                    10'b0000000_100 : ALUCtrl_o = 3'b;       // XOR
-                    10'b0000000_101 : ALUCtrl_o = 3'b;    // SRL
-                    10'b0100000_101 : ALUCtrl_o = 3'b;       // SRA
-                    10'b0000000_110 : ALUCtrl_o = 3'b;    // OR
-                    10'b0000000_111 : ALUCtrl_o = 3'b;       // AND
-                    default         : ALUCtrl_o = 3'b001;
-               endcase
-            end
-
-    2'b01 : begin // B-Type Branch 
-               ALUCtrl_o = 3'b010;//sub
-            end
+           
+    2'b10, 2'b11 : begin   // R-Type and I - Type too ????????????????? 
+                    case(funct_i[2:0])
+                        3'000 : begin 
+                                if(ALUOp_i==2'b10) // R-Type
+                                    ALUCtrl_o = (funct_i[9:3] == 0) ? 4'b0000 : 4'b0001;    // ADD : SUB
+                                else ALUCtrl_o = 4'b0000;    // ADDI                         
+                        3'001 : ALUCtrl_o = 4'b0010;    // SLL
+                        3'100 : ALUCtrl_o = 4'b0011;    // XOR
+                            3'101 : ALUCtrl_o = (funct_i[9:3] == 0) ? 4'b0100 : 4'b0101 ;    // SRL : SRA
+                        3'110 : ALUCtrl_o = 4'b0110;    // ORI
+                        3'111 : ALUCtrl_o = 4'b0111;    // ANDI
+                        default : ALUCtrl_o = 4'b0000; // default add ??
+                    endcase
+                  end
+    2'b01       : begin // B-Type Branch 
+                   case(funct_i[2:0])
+                        3'000 : ALUCtrl_o = 4'b0001;    // BEQ                
+                        3'001 : ALUCtrl_o = 4'b0001;    // BNE
+                        3'100 : ALUCtrl_o = 4'b1000;    // BLT signed 
+                        3'101 : ALUCtrl_o = 4'b1001;    // BGE sgined
+                        default : ALUCtrl_o = 4'b0001;
+                   endcase
+                 end
     default : begin
-                ALUCtrl_o = 3'b001;
+                ALUCtrl_o = 4'b0000;
               end
-endcase
+  endcase
 
 end
 
