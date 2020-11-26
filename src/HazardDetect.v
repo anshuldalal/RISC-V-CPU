60b9044b77e390491913a33354d4365dc007d348
anshuldalal <34630957+anshuldalal@users.noreply.github.com>
2020-11-26 19:47:19 +0530

    Update HazardDetect.v

--- src/HazardDetect.v
+++ src/HazardDetect.v
@@ -1,8 +1,10 @@
 module HazradDetect
 (
-    input       ID_EX_MemRead_i,
-    input [4:0] IF_ID_RS_i, IF_ID_RT_i, ID_EX_RD_i,
-    output      Hazard_o   // stall
+    input        ID_EX_MemRead_i,
+    input  [4:0] IF_ID_RS_i, 
+    input  [4:0] IF_ID_RT_i, 
+    input  [4:0] ID_EX_RD_i,
+    output       Hazard_o   // stall
 );
     // logic to detect load-use data hazard and stall the pipeline
     assign Hazard_o = ((ID_EX_MemRead_i && (ID_EX_RD_i == IF_ID_RS_i || ID_EX_RD_i == IF_ID_RT_i))? 1'b1 : 1'b0);
